0aa406e03fe475ab52334820ef15bb7ccc5803d3
niharshah <nihar.shah@deptagency.com>
2025-11-21 17:53:55 +0530

    added the embed logic and created separate blockJSON

--- tools/split-components.js
+++ tools/split-components.js
@@ -22,23 +22,23 @@ const filters = readJson(filtersPath);
 // flatten component definitions
 const defComponents = [];
 if (def && Array.isArray(def.groups)) {
-  def.groups.forEach(g => {
-    if (Array.isArray(g.components)) g.components.forEach(c => defComponents.push(c));
+  def.groups.forEach((g) => {
+    if (Array.isArray(g.components)) g.components.forEach((c) => defComponents.push(c));
   });
 }
 
-const defMap = new Map(defComponents.map(d => [d.id, d]));
-const modelsMap = new Map((models || []).map(m => [m.id, m]));
-const filtersMap = new Map((filters || []).map(f => [f.id, f]));
+const defMap = new Map(defComponents.map((d) => [d.id, d]));
+const modelsMap = new Map((models || []).map((m) => [m.id, m]));
+const filtersMap = new Map((filters || []).map((f) => [f.id, f]));
 
 // list block directories
 const blockDirs = fs.readdirSync(blocksDir, { withFileTypes: true })
-  .filter(d => d.isDirectory())
-  .map(d => d.name);
+  .filter((d) => d.isDirectory())
+  .map((d) => d.name);
 
 console.log('Blocks found:', blockDirs.length);
 
-blockDirs.forEach(blockId => {
+blockDirs.forEach((blockId) => {
   const out = { definitions: [], models: [], filters: [] };
 
   // include direct definition by id
@@ -49,7 +49,7 @@ blockDirs.forEach(blockId => {
     const f = filtersMap.get(blockId);
     out.filters.push(f);
     if (Array.isArray(f.components)) {
-      f.components.forEach(compId => {
+      f.components.forEach((compId) => {
         if (defMap.has(compId)) out.definitions.push(defMap.get(compId));
         if (modelsMap.has(compId)) out.models.push(modelsMap.get(compId));
       });
@@ -57,12 +57,12 @@ blockDirs.forEach(blockId => {
   }
 
   // include definitions that have template.model or template.filter matching blockId
-  defComponents.forEach(d => {
+  defComponents.forEach((d) => {
     try {
       const tpl = d.plugins && d.plugins.xwalk && d.plugins.xwalk.page && d.plugins.xwalk.page.template;
       if (!tpl) return;
       if (tpl.model === blockId || tpl.filter === blockId) {
-        if (!out.definitions.find(x => x.id === d.id)) out.definitions.push(d);
+        if (!out.definitions.find((x) => x.id === d.id)) out.definitions.push(d);
       }
     } catch (e) {
       // ignore
@@ -70,29 +70,29 @@ blockDirs.forEach(blockId => {
   });
 
   // include common related models: blockId, blockId-item
-  ['','-item'].forEach(suffix => {
+  ['', '-item'].forEach((suffix) => {
     const id = blockId + (suffix === '' ? '' : suffix);
-    if (modelsMap.has(id) && !out.models.find(m => m.id === id)) out.models.push(modelsMap.get(id));
-    if (defMap.has(id) && !out.definitions.find(d => d.id === id)) out.definitions.push(defMap.get(id));
+    if (modelsMap.has(id) && !out.models.find((m) => m.id === id)) out.models.push(modelsMap.get(id));
+    if (defMap.has(id) && !out.definitions.find((d) => d.id === id)) out.definitions.push(defMap.get(id));
   });
 
   // If any definitions reference other model ids via template.model, include those models
-  out.definitions.forEach(d => {
+  out.definitions.forEach((d) => {
     try {
       const tpl = d.plugins && d.plugins.xwalk && d.plugins.xwalk.page && d.plugins.xwalk.page.template;
-      if (tpl && tpl.model && modelsMap.has(tpl.model) && !out.models.find(m => m.id === tpl.model)) {
+      if (tpl && tpl.model && modelsMap.has(tpl.model) && !out.models.find((m) => m.id === tpl.model)) {
         out.models.push(modelsMap.get(tpl.model));
       }
-      if (tpl && tpl.filter && filtersMap.has(tpl.filter) && !out.filters.find(ff => ff.id === tpl.filter)) {
+      if (tpl && tpl.filter && filtersMap.has(tpl.filter) && !out.filters.find((ff) => ff.id === tpl.filter)) {
         out.filters.push(filtersMap.get(tpl.filter));
       }
     } catch (e) {}
   });
 
   // dedupe by id
-  out.definitions = out.definitions.filter((v, i, a) => a.findIndex(x => x.id === v.id) === i);
-  out.models = out.models.filter((v, i, a) => a.findIndex(x => x.id === v.id) === i);
-  out.filters = out.filters.filter((v, i, a) => a.findIndex(x => x.id === v.id) === i);
+  out.definitions = out.definitions.filter((v, i, a) => a.findIndex((x) => x.id === v.id) === i);
+  out.models = out.models.filter((v, i, a) => a.findIndex((x) => x.id === v.id) === i);
+  out.filters = out.filters.filter((v, i, a) => a.findIndex((x) => x.id === v.id) === i);
 
   const dest = path.join(blocksDir, blockId, `_${blockId}.json`);
   writeJson(dest, out);
