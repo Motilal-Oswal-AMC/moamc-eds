d9f217c491618bdcabc261d5d331e5f7ae5a069d
Kush Rathod <kush.rathod@deptagency.com>
2025-11-28 00:07:18 +0530

    loading eager add

--- scripts/aem.js
+++ scripts/aem.js
@@ -340,7 +340,7 @@ function createOptimizedPicture(
       picture.appendChild(source);
     } else {
       const img = document.createElement('img');
-      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      img.setAttribute('loading', 'eager');
       img.setAttribute('alt', alt);
       picture.appendChild(img);
       img.setAttribute(
