c3ef9410e4629a96c6f4b1213870640ea914c464
Alok Singh <Alok.Singh@teknopoint.local>
2025-12-09 11:56:36 +0530

    new comp add

--- blocks/start-building/start-building.js
+++ blocks/start-building/start-building.js
@@ -6,4 +6,22 @@ export default function decorate(block) {
     'start-section',
   ];
   dataMapMoObj.addIndexed(block);
+
+  // CHANGE THIS LINE: Use querySelectorAll
+  const download = block.querySelectorAll('.section.fab-download');
+
+  dataMapMoObj.CLASS_PREFIXES = [
+    'fab-down-cont',
+    'fab-down-sec',
+    'fab-down-sub',
+    'fab-down-inner-text',
+    'fab-down-list',
+    'fab-down-list-content',
+    'fab-down-list-row',
+  ];
+
+  // NOW this works because 'download' is a list
+  if (download.length > 0) {
+    download.forEach((sublist) => dataMapMoObj.addIndexed(sublist));
+  }
 }
