d95e67af386da22598c48f0d39d99a31c10bfb14
Kush Rathod <kush.rathod@deptagency.com>
2025-12-09 17:44:01 +0530

    bug fix

--- blocks/fund-video/fund-video.js
+++ blocks/fund-video/fund-video.js
@@ -7,15 +7,19 @@ export default function decorate(block) {
   dataMapMoObj.CLASS_PREFIXES = ['fundvedmain', 'fundvedsubmain', 'fundvedinnermain', 'fundsubvedmain'];
   dataMapMoObj.addIndexed(block);
 
-  const imgWrapper = document.createElement('div');
-  imgWrapper.classList.add('fundsubinner');
-  const fundv2 = block.querySelector('.fundvedmain2');
-  const fundvedone = block.querySelector('.fundvedmain2 .fundvedinnermain1');
-  const fundvedtwo = block.querySelector('.fundvedmain2 .fundvedinnermain2');
-  imgWrapper.appendChild(fundvedone);
-  imgWrapper.appendChild(fundvedtwo);
-  fundv2.prepend(imgWrapper);
+  try {
+    const imgWrapper = document.createElement('div');
+    imgWrapper.classList.add('fundsubinner');
+    const fundv2 = block.querySelector('.fundvedmain2');
+    const fundvedone = block.querySelector('.fundvedmain2 .fundvedinnermain1');
+    const fundvedtwo = block.querySelector('.fundvedmain2 .fundvedinnermain2');
+    imgWrapper.appendChild(fundvedone);
+    imgWrapper.appendChild(fundvedtwo);
+    fundv2.prepend(imgWrapper);
 
-  const imgAltmain = block.closest('main');
-  dataMapMoObj.altFunction(imgAltmain.querySelector('.fund-video-container .fundvedmain2 .fundvedinnermain2 img'), 'video-play-btn');
+    const imgAltmain = block.closest('main');
+    dataMapMoObj.altFunction(imgAltmain.querySelector('.fund-video-container .fundvedmain2 .fundvedinnermain2 img'), 'video-play-btn');
+  } catch (error) {
+    // console.log(error);
+  }
 }
