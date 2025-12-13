6d150b6701b451b080ef9c5ddc15bd83d68fa5c1
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-12-13 13:49:36 +0530

    fix

---
+++ blocks/your-wealth-journey/your-wealth-journey.js
@@ -0,0 +1,56 @@
+import {
+  div, input, label, img,
+} from '../../scripts/dom-helpers.js';
+
+import dataMapMoObj from '../../scripts/constant.js';
+
+export default function decorate(block) {
+  dataMapMoObj.CLASS_PREFIXES = [
+    'wealth-form',
+    'wealth-form-inner',
+    'wealth-form-inner-sub',
+    'wealth-form-sub',
+    'wealth-formsub-inner',
+  ];
+  dataMapMoObj.addIndexed(block);
+
+  const indImg = block.querySelector('.wealth-formsub-inner1 img').src;
+  const contCode = block.querySelector('.wealth-form-inner-sub3 .wealth-form-sub2');
+  const numLable = block.querySelector('.wealth-form-inner-sub3 .wealth-form-sub3');
+  const emailLable = block.querySelector('.wealth-form-inner-sub3 .wealth-form-sub4');
+  const submitBtn = block.querySelector('.wealth-form-inner-sub3 .wealth-form-sub5');
+
+  const wealthFormModal = div(
+    { class: 'form-container' },
+    div(
+      { class: 'num-cont-wrapper' },
+      div(
+        { class: 'cont-wrapper' },
+        div(
+          { class: 'ind-svg' },
+          img({ class: 'ind-img', src: indImg }),
+        ),
+        div({ class: 'cont-code' }, contCode),
+      ),
+      div(
+        { class: 'num-label-inp' },
+        label({ class: 'num-label' }, numLable),
+        input({ class: 'num-input' }),
+      ),
+
+    ),
+    div(
+      { class: 'email-submit-wrapper' },
+      div(
+        { class: 'email-label-inp' },
+        label({ class: 'email-label' }, emailLable),
+        input({ class: 'email-imput' }),
+      ),
+      div({ class: 'submit-btn' }, submitBtn),
+    ),
+  );
+
+  const formData = block.querySelector('.wealth-form-inner-sub3');
+  formData.innerHTML = '';
+  formData.appendChild(wealthFormModal);
+}
